b46be017c9420883350828da0bdff748f12161c1
Balsa Asanovic <balsaasanovic95@gmail.com>
2023-10-01 22:40:14 +0200

    added emitters mechanics

--- Scripts/Emitters.gd
+++ Scripts/Emitters.gd
@@ -6,7 +6,17 @@ var limit = 0
 
 @onready var global = get_node("/root/Global")
 
-var choices = []
+var choices = [ preload("res://Scenes/blueProjectile.tscn"), 
+				preload("res://Scenes/greenProjectile.tscn"), 
+				preload("res://Scenes/redProjectile.tscn"),
+				preload("res://Scenes/purpleProjectile.tscn"),
+				preload("res://Scenes/orangeProjectile.tscn"),
+				preload("res://Scenes/yellowProjectile.tscn"),
+				preload("res://Scenes/diffGreenProjectile.tscn"),
+				preload("res://Scenes/diffBlueProjectile.tscn"),
+				preload("res://Scenes/diffRedProjectile.tscn") ]
+
+var emmiters = [get_node("LeftEmitter"), get_node("MiddleEmitter"), get_node("RightEmitter")]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,3 +26,34 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func begin_game():
+	timer = get_node("Timer")
+	timer.set_wait_time(global.emit_time)
+	timer.start()
+
+
+func _on_timer_timeout():
+	randomize()
+	var projectile = choices[int(randf_range(0, global.projectile_limit))].instance()
+	add_child(projectile)
+	
+	randomize()
+	var emit = int(randf_range(1, 4))
+	if emit == 1:
+		projectile.position = get_node("LeftEmitter").position
+	if emit == 2:
+		projectile.position = get_node("MiddleEmitter").position
+	if emit == 3:
+		projectile.position = get_node("RightEmitter").position
+
+	projectile.y_speed = int(global.projectile_speed)
+	if (global.game_over == false):
+		timer.set_wait_time(global.emit_time)
+		timer.start()
+	else:
+		timer.stop()
+
+func move(target):
+	var move_tween = create_tween()
+	move_tween.tween_property(self, "position", target, 1)
